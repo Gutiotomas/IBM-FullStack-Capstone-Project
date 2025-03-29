f49cf00a867353ac1947903784e6231dde80e444
Gutiotomas <tgutierrez@unal.edu.co>
2025-03-29 19:10:59 -0400

    Connect to MongoDB

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,20 +9,23 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    try {
+        // Task 1: Connect to MongoDB
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+        // Task 2: Connect to database giftDB and store in variable dbInstance
+        dbInstance = client.db(dbName);
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+        // Task 3: Return database instance
+        return dbInstance;
+    } catch (error) {
+        console.error("Failed to connect to the database", error);
+        throw error;
+    }
 }
 
 module.exports = connectToDatabase;
