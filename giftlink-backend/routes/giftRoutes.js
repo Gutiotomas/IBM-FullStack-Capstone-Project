48c9f2dfb0b46baa40ff339abbec7b7fdc657e06
Gutiotomas <tgutierrez@unal.edu.co>
2025-03-30 03:16:10 -0400

    Complete app

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connectToDatabase = require('./db');
+const connectToDatabase = require('../models/db');
 
 // Get all gifts
 router.get('/', async (req, res) => {
